6212a9151c1b6fc2b6389a4bf0743bbf400df5a4
Oğuzhan <oguzhnural@gmail.com>
2025-02-09 20:34:49 +0300

    Update next congiguration

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:4000/:path*',
+      },
+    ];
+  },
+  server: {
+    port: 3001,
+  },
+}
+
+module.exports = nextConfig
