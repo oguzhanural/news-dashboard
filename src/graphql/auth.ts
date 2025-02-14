5fb1bce74d287a75c800c60d95cb330c56085217
Oğuzhan <oguzhnural@gmail.com>
2025-02-14 16:44:52 +0300

    Fix graphql mutation mistmatch between frontend and backend

--- src/graphql/auth.ts
+++ src/graphql/auth.ts
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_MUTATION = gql`
-  mutation Login($input: LoginInput!) {
-    login(input: $input) {
+  mutation loginUser($input: LoginUserInput!) {
+    loginUser(input: $input) {
       token
       user {
         id
@@ -16,7 +16,7 @@ export const LOGIN_MUTATION = gql`
 `;
 
 export const REGISTER_USER_MUTATION = gql`
-  mutation RegisterUser($input: RegisterUserInput!) {
+  mutation registerUser($input: RegisterUserInput!) {
     registerUser(input: $input) {
       token
       user {
