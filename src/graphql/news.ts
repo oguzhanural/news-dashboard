3a75ff7e380f7cda7381dea6c5aaa8e1256c58ac
Oğuzhan <oguzhnural@gmail.com>
2025-03-06 16:23:53 +0300

    Update get categories query

--- src/graphql/news.ts
+++ src/graphql/news.ts
@@ -8,20 +8,18 @@ export const CREATE_NEWS_MUTATION = gql`
       content
       summary
       slug
-      category {
-        id
-        name
-      }
+      status
+      publishDate
+      createdAt
+      tags
       author {
         id
         name
       }
-      status
-      tags
-      images
-      publishDate
-      createdAt
-      updatedAt
+      category {
+        id
+        name
+      }
     }
   }
 `;
@@ -34,47 +32,84 @@ export const UPDATE_NEWS_MUTATION = gql`
       content
       summary
       slug
+      status
+      publishDate
+      updatedAt
+      tags
+      author {
+        id
+        name
+      }
       category {
         id
         name
       }
-      status
-      tags
-      images
-      publishDate
-      updatedAt
     }
   }
 `;
 
 export const DELETE_NEWS_MUTATION = gql`
   mutation DeleteNews($id: ID!) {
-    deleteNews(id: $id)
+    deleteNews(id: $id) {
+      id
+    }
   }
 `;
 
 export const GET_NEWS_QUERY = gql`
-  query GetNews($id: ID!) {
-    news(id: $id) {
+  query GetNews($page: Int, $limit: Int, $status: String) {
+    getAllNews(page: $page, limit: $limit, status: $status) {
+      total
+      page
+      limit
+      items {
+        id
+        title
+        summary
+        status
+        publishDate
+        createdAt
+        author {
+          id
+          name
+        }
+        category {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
+export const GET_NEWS_ITEM_QUERY = gql`
+  query GetNewsItem($id: ID!) {
+    getNewsById(id: $id) {
       id
       title
       content
       summary
       slug
-      category {
-        id
-        name
+      status
+      publishDate
+      createdAt
+      updatedAt
+      tags
+      images {
+        url
+        isMain
+        caption
+        altText
+        credit
       }
       author {
         id
         name
       }
-      status
-      tags
-      images
-      publishDate
-      createdAt
-      updatedAt
+      category {
+        id
+        name
+      }
     }
   }
 `;
@@ -110,10 +145,20 @@ export const GET_ALL_NEWS_QUERY = gql`
 `;
 
 export const GET_CATEGORIES_QUERY = gql`
-  query GetCategories {
+  query {
     categories {
       id
       name
+      slug
+    }
+  }
+`;
+
+export const DELETE_CLOUDINARY_IMAGE_MUTATION = gql`
+  mutation DeleteCloudinaryImage($url: String!) {
+    deleteCloudinaryImage(url: $url) {
+      success
+      message
     }
   }
 `; 
