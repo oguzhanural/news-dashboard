16da5bc0b3369ede4e8ca94e52de1c22edf1ae05
Oğuzhan <oguzhnural@gmail.com>
2025-03-21 15:22:27 +0300

    Import ToastProvider

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -3,12 +3,14 @@
 import { ApolloWrapper } from '@/lib/apollo-wrapper';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { CloudinaryProvider } from './CloudinaryProvider';
+import { ToastProvider } from './ToastProvider';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
       <ApolloWrapper>
         <CloudinaryProvider>
+          <ToastProvider />
           {children}
         </CloudinaryProvider>
       </ApolloWrapper>
